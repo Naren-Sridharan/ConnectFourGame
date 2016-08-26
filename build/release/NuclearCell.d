40beeb8c09c7e6a615d5c58002c40daa5e2700db
Ashwin-Surana <surana.an@gmail.com>
2016-08-26 18:13:55 +0530

    added gitignore

--- build/release/NuclearCell.d
+++
@@ -1,6 +0,0 @@
-build/release/NuclearCell.o: src/NuclearCell.cpp include/NuclearCell.h \
-  include/Constants.h
-
-include/NuclearCell.h:
-
-include/Constants.h:
--- build/release/NuclearReactor.d
+++
@@ -1,8 +0,0 @@
-build/release/NuclearReactor.o: src/NuclearReactor.cpp \
-  include/NuclearReactor.h include/NuclearCell.h include/Constants.h
-
-include/NuclearReactor.h:
-
-include/NuclearCell.h:
-
-include/Constants.h:

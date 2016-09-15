93cd9a7c35d40a7d093490e8ac1bebdfe4563ea3
Naren-Jegan <naren.jegan26@gmail.com>
2016-09-15 18:12:21 +0530

    C++ only

--- Python/Bot.py
+++
@@ -1,9 +0,0 @@
-import NuclearReactor
-
-class Bot(object):
-    def __init__(self,ptype):
-        self_ptype = ptype
-
-    def calculateNextMove(self,cb):
-        """Calculating next move"""
-        return
--- Python/Constants.py
+++
@@ -1,9 +0,0 @@
-def enum(**named_values):
-    return type('Enum', (), named_values)
-class Constants:
-    compbotname = "Defuser"
-    userbotname = "bomber"
-    ROWS = 6
-    COLUMNS = 7
-    NuclearCellStatus = enum(BOMB = 0,DEFUSER = 1, EMPTY = 2, INVALID = 3)
-    PlayerType = enum(USER = 0,COMP = 1, NOPLAYER = 2)
